32cfc8d3600602c18294022c3e1c41b1d0bf6280
Kabaye <svkulich@gmail.com>
2020-01-30 21:38:42 +0300

    fix: fix port for heroku

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -23,7 +23,7 @@ function connect(event) {
         usernamePage.classList.add('hidden');
         chatPage.classList.remove('hidden');
 
-        const socket = new WebSocket('ws://0.0.0.0:18220/ws');
+        const socket = new WebSocket('wss://wsch.herokuapp.com//ws');
         stompClient = Stomp.over(socket);
 
         stompClient.connect({}, onConnected, onError);
