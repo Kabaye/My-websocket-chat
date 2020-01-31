44e61d3ab333938e54ab1607ffcc37c319aa218e
svku0919 <svkulich@gmail.com>
2020-01-31 11:24:32 +0300

    feat: add node.js

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -23,7 +23,7 @@ function connect(event) {
         usernamePage.classList.add('hidden');
         chatPage.classList.remove('hidden');
 
-        const socket = new WebSocket('ws://localhost:18220/ws');
+        const socket = new WebSocket('ws://localhost:' + port + '/ws');
         stompClient = Stomp.over(socket);
 
         stompClient.connect({}, onConnected, onError);
